ce23209fcb7b4258373e4f8a8c495e7ccc4211d8
Kirito99x <msaifeldin46@gmail.com>
2024-04-20 09:01:07 +0200

    fixed

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,13 +2,14 @@
 """start a flask web-app"""
 
 from flask import Flask
-app =Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def index():
     """return a Hello HBNB"""
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
