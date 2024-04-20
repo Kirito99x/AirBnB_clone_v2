389e794dbcc7176803211ea1d6a85019b2344066
Kirito99x <msaifeldin46@gmail.com>
2024-04-20 10:14:49 +0200

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""start a flask web-app display c is fun"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def Greeting():
+    """return a Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def display_HBNB():
+    """Display HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_C(text):
+    """Display C is fun"""
+    return "C " + text.replace('_', ' ')
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python/", strict_slashes=False)
+def display_python(text="is cool"):
+    """Display python is fun"""
+    return "python " + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
